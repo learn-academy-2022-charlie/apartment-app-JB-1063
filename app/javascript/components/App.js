8995764b95239c4479e4c2c6d77b7827eac0eeaa
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-07-07 19:53:04 -0700

    navigation completed

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -21,7 +21,7 @@ class App extends Component {
           <Header {...this.props} />
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/apartmentindex" component={ApartmentIndex} />
+            <Route path="/apartmentindex" render={(props) =>  <ApartmentIndex  apartments={this.state.apartments} /> } />
             <Route path="/apartmentshow" component={ApartmentShow} />
             <Route path="/apartmentnew" component={ApartmentNew} />
             <Route path="/apartmentedit" component={ApartmentEdit} />
